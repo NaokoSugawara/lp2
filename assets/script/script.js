8313d4e6ae7c2b6577f37b6619489725a93e3259
naoko.sugawara <infoseek122@gmail.com>
2025-01-22 10:48:07 -0500

    added QnA to sp

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,11 +3,11 @@ const accordionTitles = document.querySelectorAll(".accordion__title");
 accordionTitles.forEach((accordionTitle) => {
 
     accordionTitle.addEventListener("click", () => {
-        accordionTitle.classList.toggle("accordion__title--active");
-        // accordionTitle.parentElement.classList.toggle("according__item--active");
+        const icon = accordionTitle.children[1];
+        icon.classList.toggle("accordion__title__icon--active");
 
         let height = accordionTitle.nextElementSibling.scrollHeight;
-        if (accordionTitle.classList.contains("accordion__title--active")) {
+        if (icon.classList.contains("accordion__title__icon--active")) {
             accordionTitle.nextElementSibling.style.maxHeight = `${height}px`;
         } else {
             accordionTitle.nextElementSibling.style.maxHeight = "0px";
